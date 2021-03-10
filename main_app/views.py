f3c8458f2f1f5203f8c17bede55d3f433798c08c
howardnguyen714 <howardnguyen714@gmail.com>
2021-03-09 16:53:54 -0800

    added admin functionality, index and details page for dogs, and implemented postgresql

--- main_app/views.py
+++ main_app/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from .models import Dog
 from django.http import HttpResponse
 
 # Create your views here.
@@ -9,3 +10,11 @@ def home(request):
 
 def about(request):
     return render(request, 'about.html')
+
+def dogs_index(request):
+    dogs = Dog.objects.all()
+    return render(request, 'dogs/index.html', { 'dogs': dogs })
+
+def dogs_detail(request, dog_id):
+    dog = Dog.objects.get(id = dog_id)
+    return render(request, 'dogs/detail.html', { 'dog': dog })
