57d202b26b1b65d9fa00bdc9ae9af5badbc253e5
howardnguyen714 <howardnguyen714@gmail.com>
2021-03-12 15:45:12 -0800

    added feed functionality

--- main_app/models.py
+++ main_app/models.py
@@ -1,8 +1,30 @@
 from django.db import models
 
+MEALS = (
+    ('B', 'Breakfast'),
+    ('L', 'Lunch'),
+    ('D', 'Dinner')
+)
+
 # Create your models here.
 class Dog(models.Model):
     name = models.CharField(max_length=100)
     breed = models.CharField(max_length=100)
     description = models.TextField(max_length=250)
     age = models.IntegerField()
+
+class Feeding(models.Model):
+    date = models.DateField('feeding date')
+    meal = models.CharField(
+        max_length=1,
+        choices=MEALS,
+        default=MEALS[0][0]
+    )
+
+    dog = models.ForeignKey(Dog, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f"{self.get_meal_display()} on {self.date}"
+    
+    class Meta:
+        ordering = ['-date']
--- main_app/urls.py
+++ main_app/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
     path('about/', views.about, name='about'),
     path('dogs/', views.dogs_index, name='index'),
     path('dogs/<int:dog_id>/', views.dogs_detail, name='detail'),
+    path('dogs/<int:dog_id>/add_feeding/', views.add_feeding, name='add_feeding'),
 ]
